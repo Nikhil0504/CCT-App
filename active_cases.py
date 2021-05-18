ff0bda8c919042068afc40250c6aa91aafa73e7d
Nikhil Garuda <34582615+Nikhil0504@users.noreply.github.com>
2021-05-18 20:29:48 +0530

    Made the new tables

--- active_cases.py
+++ active_cases.py
@@ -9,8 +9,8 @@ st.markdown("The dashboard will visualize the Covid-19 Situation in India")
 st.markdown(
     "Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus. Most people infected with the COVID-19 virus will experience mild to moderate respiratory illness and recover without requiring special treatment."
 )
-# st.sidebar.title("Visualization Selector")
-# st.sidebar.markdown("Select the Charts/Plots accordingly:")
+st.sidebar.title("Visualization Selector")
+st.sidebar.markdown("Select the Charts/Plots accordingly:")
 
 a = requests.get(
     "https://api.apify.com/v2/key-value-stores/toDWvRj1JpTXiM8FF/records/LATEST?disableRedirect=true.json"
@@ -30,11 +30,11 @@ rename_mapping = {
 }
 b.rename(columns=rename_mapping, inplace=True)
 
-# select = st.sidebar.selectbox(
-#    "Cases type",
-#    ["", "Active Cases", "Recovered Cases", "Deceased Cases", "Total Infected Cases"],
-#    key="3",
-# )
+ select = st.sidebar.selectbox(
+    "Cases type",
+    ["", "Active Cases", "Recovered Cases", "Deceased Cases", "Total Infected Cases"],
+    key="3",
+ )
 
 
 def table(b):
@@ -46,27 +46,21 @@ def table(b):
     return T
 
 
-# if select == "":
-#    st.table(b)
-#
-# if select == "Active Cases":
-#    table(b)
-#    st.bar_chart(b["Active Cases"])
-#
-# if select == "Recovered Cases":
-#    table(b)
-#    st.bar_chart(b["Active Cases"])
-#
-# if select == "Deceased Cases":
-#    table(b)
-#    st.bar_chart(b["Deceased Cases"])
-#
-# if select == "Total Infected Cases":
-#    table(b)
-#    st.bar_chart(b["Total Infected Cases"])
+ if select == "":
+    st.table(b)
 
-table(b)
-st.bar_chart(b["Active Cases"])
-st.bar_chart(b["Active Cases"])
-st.bar_chart(b["Deceased Cases"])
-st.bar_chart(b["Total Infected Cases"])
+ if select == "Active Cases":
+    table(b)
+    st.bar_chart(b["Active Cases"])
+
+ if select == "Recovered Cases":
+    table(b)
+    st.bar_chart(b["Active Cases"])
+
+ if select == "Deceased Cases":
+    table(b)
+    st.bar_chart(b["Deceased Cases"])
+
+ if select == "Total Infected Cases":
+    table(b)
+    st.bar_chart(b["Total Infected Cases"])
